54f2206ca7761884a3677c9ef97acf0154de959e
fckuligowski <frank.kuligowski@thinkahead.com>
2020-05-28 12:17:47 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Travelin' in the Dark"
+    return "Two Babies in the Dark"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
